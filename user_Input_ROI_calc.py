9bbfe90821cca7cea0d793f2e7744343525a991f
cosovanalex <47539883+cosovanalex@users.noreply.github.com>
2022-09-28 22:40:19 -0500

    Add files via upload

--- user_Input_ROI_calc.py
+++ user_Input_ROI_calc.py
@@ -1,9 +1,23 @@
-Income = int(input("\nWhat is your Total Monthly Income?\nThis includes things like:\n \tRental Income\n \tLaundry Income\n \tStorage Income\n \tMiscellanious Income the property brings in\nEnter amount: "))
-Expenses = int(input("\nWhat are your Total Monthly Expenses?\nThis includes things like:\n \tTaxes\n \tInsurance\n \tWater/sewer\n \tGarbage\n \tElectric\n \tGas\n \tHOA Fees\n \tLawn/Snow\n \tVacancy\n \tRepairs\n \tCapEx\n \tProperty Management\n \tMortgagen\nEnter amount: "))
-Investments = int(input("\nWhat is the total investment in the property?\nThis includes things like:\n \tDown Payment amount\n \tClosing Costs amount\n \tRehab Budget\n \tMisc Other\nEnter amount: "))
-def ROI(Income, Expenses, Investments):
-    CashFlow = (Income - Expenses) * 12
-    ROI = (CashFlow/Investments) * 100
-    print (f"\nYour Cash on Cash Return is {ROI}%")
+class RoiCalculator:
+    
+    def __init__(self, income, expenses, investments):
+        self.income = income
+        self.expenses = expenses
+        self.investments = investments
+    @staticmethod    
+    def roi_calc(CashFlow, investments):
+        
+        x = (int(CashFlow) / int(investments)) * 100
+        print (f"\nYour Cash on Cash Return is {x}%")
 
-ROI(Income, Expenses, Investments)
+income = int(input("\nWhat is your Total Monthly Income?\nThis includes things like:\n \tRental Income\n \tLaundry Income\n \tStorage Income\n \tMiscellanious Income the property brings in\nEnter amount: "))
+expenses = int(input("\nWhat are your Total Monthly Expenses?\nThis includes things like:\n \tTaxes\n \tInsurance\n \tWater/sewer\n \tGarbage\n \tElectric\n \tGas\n \tHOA Fees\n \tLawn/Snow\n \tVacancy\n \tRepairs\n \tCapEx\n \tProperty Management\n \tMortgagen\nEnter amount: "))
+investments = int(input("\nWhat is the total investment in the property?\nThis includes things like:\n \tDown Payment amount\n \tClosing Costs amount\n \tRehab Budget\n \tMisc Other\nEnter amount: "))
+CashFlow = (income - expenses) * 12
+
+
+p1 = RoiCalculator(income, expenses, investments)
+p1.roi_calc(CashFlow, investments)
+
+
+
